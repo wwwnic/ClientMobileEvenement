17c78e53bef217318e7019f2d8d6311497784dd6
Nicolas Brunet <1816098@crosemont.qc.ca>
2021-10-21 12:58:14 -0400

    ajout structure

---
+++ app/src/main/kotlin/com/even/ui/activité/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.even.ui.activité
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.even.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
