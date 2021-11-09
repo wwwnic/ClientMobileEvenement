ab4b8a19ccff626914a4a805a67e5eb264305541
Nicolas Brunet <1816098@crosemont.qc.ca>
2021-11-08 22:56:58 -0500

    Ajout du fragment connexion ainsi que ses dépendances (values/drawable)

--- app/src/main/java/com/even/ui/fragment/list_evenement.kt
+++ app/src/main/java/com/even/ui/fragment/list_evenement.kt
@@ -1,10 +1,16 @@
 package com.even.ui.fragment
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 import com.even.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,11 +28,19 @@ class liste_evenement : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
+
+            val listView = view?.findViewById<ListView>(R.id.listEvenement_listEvenement) as ListView
+//        val redColor = Color.parseColor("#FF0000")
+//        listView.setBackgroundColor(redColor)
+
+            listView.adapter = MyCustomAdapter(this.context as Context) // this needs to be my custom adapter telling my list what to render
         }
     }
 
@@ -35,7 +49,7 @@ class liste_evenement : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_liste_evenement, container, false)
+        return inflater.inflate(R.layout.fragment_list_evenement, container, false)
     }
 
     companion object {
@@ -56,5 +70,38 @@ class liste_evenement : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+
+    private class MyCustomAdapter(context: Context): BaseAdapter() {
+
+        private val mContext: Context
+
+        init {
+            mContext = context
+        }
+
+        // responsible for how many rows in my list
+        override fun getCount(): Int {
+            return 5
+        }
+
+        // you can also ignore this
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        // you can ignore this for now
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        // responsible for rendering out each row
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "HERE is my ROW for my LISTVIEW"
+            return textView
+        }
+
     }
 }
