240beea384cd15f946f98bcdcc51cb0b980d5279
Olivier Gauthier <ogauthier@dept-info.crosemont.quebec>
2021-11-22 19:02:51 +0000

    Revert "Merge branch 'recit_enregistrement' into 'main'"
    This reverts merge request !15

--- app/src/main/java/com/even/domaine/interacteur/IEnregistrement.kt
+++ app/src/main/java/com/even/domaine/interacteur/IEnregistrement.kt
@@ -1,35 +1,17 @@
 package com.even.domaine.interacteur
 
 interface IEnregistrement {
+
     interface IVue {
         fun naviguerVersConnexion()
-        fun afficherToastSuccesEnregistrement()
-        fun afficherToastErreurEnregistrement()
-        fun afficherToastErreurServeur()
-        fun afficherErreurNomUsager(afficherEnRouge: Boolean)
-        fun afficherErreurMotDePasse(afficherEnRouge: Boolean)
-        fun afficherErreurCourriel(afficherEnRouge: Boolean)
-        fun afficherErreurTéléphone(afficherEnRouge: Boolean)
     }
 
     interface IPrésentateur {
         fun traiterRequêteReclamerEnregistrement(
-            nomUsager: CharSequence,
-            motDePasse: CharSequence,
-            courriel: CharSequence,
-            telephone: CharSequence
+            username: String,
+            password: String,
+            email: String,
+            phone: String
         )
-
-        fun traiterRequêteValiderTousLesEntrées(
-            nomUsager: CharSequence,
-            motDePasse: CharSequence,
-            courriel: CharSequence,
-            telephone: CharSequence
-        ): Boolean
-
-        fun traiterRequêteValiderNomUsager(nomUsager: CharSequence): Boolean
-        fun traiterRequêteValiderMotDePasse(motDePasse: CharSequence): Boolean
-        fun traiterRequêteValiderCourriel(courriel: CharSequence): Boolean
-        fun traiterRequêteValiderTelephone(telephone: CharSequence): Boolean
     }
 }
--- app/src/main/java/com/even/domaine/interacteur/IntEnregistrement.kt
+++ app/src/main/java/com/even/domaine/interacteur/IntEnregistrement.kt
@@ -1,26 +1,31 @@
 package com.even.domaine.interacteur
 
 import android.util.Log
-import com.even.domaine.entité.ApiReponse
+import com.even.domaine.entité.ApiClient
+import com.even.domaine.entité.ApiClient.apiService
 import com.even.domaine.entité.Utilisateur
 import com.even.sourceDeDonnées.IApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-import retrofit2.Response
-
-class IntEnregistrement(val api: IApiService) {
+class IntEnregistrement() {
 
     suspend fun enregisterNouvelUtilisateur(
-        username: CharSequence,
-        password: CharSequence,
-        email: CharSequence,
-        phone: CharSequence
-    ): Response<ApiReponse> {
-        val reponseRequete = api.creerUtilisateur(
-            username,
-            password,
-            email,
-            phone
+        username: String,
+        password: String,
+        email: String,
+        phone: String
+    ) {
+        var reponseRequete = apiService.creerUtilisateur(
+            Utilisateur(
+                0,
+                username,
+                password,
+                email,
+                phone
+            )
         )
-        return reponseRequete
+        Log.i("Réponse POST", reponseRequete.toString()) //TODO: log
     }
 }
