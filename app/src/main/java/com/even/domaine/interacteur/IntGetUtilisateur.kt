db35409e9f9fe36f991b071456882689afe8e544
Olivier <ogauthier@dti.crosemont.quebec>
2021-12-13 09:30:49 -0500

    ajout de la doc dans IntGetUtilisateur

--- app/src/main/java/com/even/domaine/interacteur/IntGetUtilisateur.kt
+++ app/src/main/java/com/even/domaine/interacteur/IntGetUtilisateur.kt
@@ -7,14 +7,33 @@ import com.even.testOuvert
 
 @testOuvert
 class IntGetUtilisateur(var _source: ISourceDeDonnées) {
+
+    /**
+     * Méthode qui permet d'aller chercher un utilisateur à partir d'un id.
+     *
+     * @param id Clé unique qui réprésente l'utilisateur à rechercher.
+     * @return Retourne l'utilisateur qui correspons à l'id.
+     */
     suspend fun getParId(id: Int): Utilisateur? {
         return _source.getUtilisateurParId(id)
     }
 
+    /**
+     * Méthode qui permet d'aller chercher la liste d'utilisateur qui participe à un événement.
+     *
+     * @param idEvenement Clé unique qui représente l'événement sélectionné.
+     * @return Retourne la liste d'utilisateur.
+     */
     suspend fun getUtilisateursDansÉvénement(idEvenement: Int): List<Utilisateur> {
         return _source.getUtilisateursDansEvenement(idEvenement)
     }
 
+    /**
+     * Méthode qui permet d'aller chercher l'image de l'utilisateur.
+     *
+     * @param id Clé unique qui représente l'utilisateur sélectionné.
+     * @return Retourne la chaine de caractère de l'url de l'image.
+     */
     fun getImageUtilisateur(id : Int) : String {
         return _source.getImageUtilisateur(id)
     }
