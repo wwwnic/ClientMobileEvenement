814a58e83945aad530433ebbc458f9bfbc4623f2
Nicolas Brunet <1816098@crosemont.qc.ca>
2021-12-13 11:03:27 -0500

    Ajout de Kdoc

--- app/src/main/java/com/even/présentation/présenteur/IConnexion.kt
+++ app/src/main/java/com/even/présentation/présenteur/IConnexion.kt
@@ -1,16 +1,55 @@
 package com.even.présentation.présenteur
 
-import com.even.testOuvert
-
-@testOuvert
+/**
+ * Une liste de contact représentant un lien entre la vue enregistrement et le présentateur connexion
+ *
+ */
 interface IConnexion {
     interface IVue {
+        /**
+         * Navigue vers le fragment principal
+         *
+         */
         fun naviguerVersFragmentPrincipal()
+
+        /**
+         * Navigue vers le fragment enregistrement
+         *
+         */
         fun naviguerVersFragmentEnregistgrement()
+
+        /**
+         * Affiche un toast qui indique que la connexion s'est bien déroulée
+         *
+         */
         fun afficherToastSuccesConnexion()
+
+        /**
+         * Affiche un toast qui indique une erreur de connexion du à des informations invalides
+         *
+         */
         fun afficherToastErreurConnexion()
+
+        /**
+         * Affiche un toast qui indique une erreur de connexion avec le serveur
+         *
+         */
         fun afficherToastErreurServeur()
+
+        /**
+         * Change la couleur de la zone contenant le nom utilisateur afin de permettre à l'utilisateur
+         * de réaliser qu'il a fait une erreur dans le champ.
+         *
+         * @param afficherEnRouge si la zone doit etre affichée en rouge
+         */
         fun afficherErreurNomUtilisateur(afficherEnRouge: Boolean)
+
+        /**
+         * Change la couleur de la zone contenant le mot de passe afin de permettre à l'utilisateur
+         * de réaliser qu'il a fait une erreur dans le champ.
+         *
+         * @param afficherEnRouge si la zone doit etre affichée en rouge
+         */
         fun afficherErreurMotDePasse(afficherEnRouge: Boolean)
     }
 
--- app/src/main/java/com/even/présentation/présenteur/IEnregistrement.kt
+++ app/src/main/java/com/even/présentation/présenteur/IEnregistrement.kt
@@ -1,14 +1,65 @@
 package com.even.présentation.présenteur
 
+/**
+ * Une liste de contact représentant un lien entre la vue enregistrement et le présentateur enregistrement
+ *
+ */
 interface IEnregistrement {
     interface IVue {
+        /**
+         * Navigue vers l'écran connexion
+         *
+         */
         fun naviguerVersConnexion()
+
+        /**
+         * Affiche un toast qui indique que l'enregistrement s'est bien déroulée
+         *
+         */
         fun afficherToastSuccesEnregistrement()
+
+        /**
+         * Affiche un toast qui indique une erreur d'enregistrement
+         *
+         */
         fun afficherToastErreurEnregistrement()
+
+        /**
+         * Affiche un toast qui indique une erreur de connexion avec le serveur
+         *
+         */
         fun afficherToastErreurServeur()
+
+        /**
+         * Change la couleur de la zone contenant le nom utilisateur afin de permettre à l'utilisateur
+         * de réaliser qu'il a fait une erreur dans le champ.
+         *
+         * @param afficherEnRouge si la zone doit etre affichée en rouge
+         */
         fun afficherErreurNomUsager(afficherEnRouge: Boolean)
+
+        /**
+         * Change la couleur de la zone contenant le mot de passe afin de permettre à l'utilisateur
+         * de réaliser qu'il a fait une erreur dans le champ.
+         *
+         * @param afficherEnRouge si la zone doit etre affichée en rouge
+         */
         fun afficherErreurMotDePasse(afficherEnRouge: Boolean)
+
+        /**
+         * Change la couleur de la zone contenant le courriel afin de permettre à l'utilisateur
+         * de réaliser qu'il a fait une erreur dans le champ.
+         *
+         * @param afficherEnRouge si la zone doit etre affichée en rouge
+         */
         fun afficherErreurCourriel(afficherEnRouge: Boolean)
+
+        /**
+         * Change la couleur de la zone contenant le telephone afin de permettre à l'utilisateur
+         * de réaliser qu'il a fait une erreur dans le champ.
+         *
+         * @param afficherEnRouge si la zone doit etre affichée en rouge
+         */
         fun afficherErreurTéléphone(afficherEnRouge: Boolean)
     }
 
