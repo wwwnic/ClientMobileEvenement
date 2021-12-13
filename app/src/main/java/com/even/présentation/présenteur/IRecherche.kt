d4fdd7e2a48a406e81283ea4580474fbfd0d73c4
Olivier <ogauthier@dti.crosemont.quebec>
2021-12-13 16:40:53 -0500

    ajout de la doc dans VueRecherche, PrésentateurRecherche et IRecherche

--- app/src/main/java/com/even/présentation/présenteur/IRecherche.kt
+++ app/src/main/java/com/even/présentation/présenteur/IRecherche.kt
@@ -2,11 +2,31 @@ package com.even.présentation.présenteur
 
 interface IRecherche {
     interface IVue {
+
+        /**
+         * Méthode qui permet de faire l'affichage de la liste d'événement retourné après la recherche
+         *
+         * @param tag Chaine de caratère de la recherche
+         */
         fun afficherRésultatsRecherche(tag : String)
+
+        /**
+         * Méthode qui affiche simplement un toast lorsqu'aucun mot-clé n'est entré.
+         *
+         */
         fun afficherMessageAucunMotCle()
     }
 
     interface IPrésentateur {
+
+        /**
+         * Méthode qui permet de faire une recherche d'événement à partir des critères établies
+         *
+         * @param nom Nom de l'événement
+         * @param mois Mois durant lequel l'événement aura lieu
+         * @param location Endroit dans lequel l'événement aura lieu
+         * @param organisateur Organisateur de l'événement
+         */
         fun traiterRequêteRechercheÉvénement(nom : String,mois : String,location : String,organisateur : String)
     }
 }
--- app/src/main/java/com/even/présentation/présenteur/PrésentateurRecherche.kt
+++ app/src/main/java/com/even/présentation/présenteur/PrésentateurRecherche.kt
@@ -3,6 +3,15 @@ package com.even.présentation.présenteur
 class PrésentateurRecherche(
     val vue : IRecherche.IVue
 ) : IRecherche.IPrésentateur{
+
+    /**
+     * Méthode qui permet de faire une recherche d'événement à partir des critères établies
+     *
+     * @param nom Nom de l'événement
+     * @param mois Mois durant lequel l'événement aura lieu
+     * @param location Endroit dans lequel l'événement aura lieu
+     * @param organisateur Organisateur de l'événement
+     */
     override fun traiterRequêteRechercheÉvénement(
         nom: String,
         mois: String,
